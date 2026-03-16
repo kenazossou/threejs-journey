70da2a729c446d52b6885f7816a078dd456214ae
default <elcanahzossou@gmail.com>
2026-03-16 20:48:59 +0000

    Add base: '/threejs-journey/' to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import restart from 'vite-plugin-restart'
 
 export default defineConfig({
+    base: '/threejs-journey/',
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server: {
